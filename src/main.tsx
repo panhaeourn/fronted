41c8c26decb0c0b0f3f498b1a34a52eadf79bdef
panha <panhaeourn7@gmail.com>
2026-07-05 13:40:12 +0700

    Remove legacy course image cache

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,37 @@ import "./index.css";
 import "./App.css";
 import "./styles.css";
 
+const legacyCourseImageWorker = "course-image-cache-sw.js";
+const legacyCourseImageCachePrefix = "cito-course-images-";
+
+async function removeLegacyCourseImageCache() {
+  if ("serviceWorker" in navigator) {
+    const registrations = await navigator.serviceWorker.getRegistrations();
+    await Promise.all(
+      registrations
+        .filter((registration) =>
+          [registration.active, registration.waiting, registration.installing].some((worker) =>
+            worker?.scriptURL.includes(legacyCourseImageWorker)
+          )
+        )
+        .map((registration) => registration.unregister())
+    );
+  }
+
+  if ("caches" in window) {
+    const cacheNames = await caches.keys();
+    await Promise.all(
+      cacheNames
+        .filter((cacheName) => cacheName.startsWith(legacyCourseImageCachePrefix))
+        .map((cacheName) => caches.delete(cacheName))
+    );
+  }
+}
+
+void removeLegacyCourseImageCache().catch(() => {
+  // Cleanup failure must never prevent the application from starting.
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <LanguageProvider>
