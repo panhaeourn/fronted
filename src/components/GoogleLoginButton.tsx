80ae1eeacaf672faec29193e56f5d4a78cc1100a
panha <panhaeourn7@gmail.com>
2026-08-04 12:07:23 +0700

    Revert "Remove Google icon background"
    This reverts commit 5324d42bd6492e3e1d5d5a30a806f0dd673d1844.

--- src/components/GoogleLoginButton.tsx
+++ src/components/GoogleLoginButton.tsx
@@ -34,17 +34,20 @@ const buttonStyle: React.CSSProperties = {
 };
 
 const iconWrapStyle: React.CSSProperties = {
-  width: 24,
-  height: 24,
+  width: 28,
+  height: 28,
+  borderRadius: 999,
   display: "inline-flex",
   alignItems: "center",
   justifyContent: "center",
+  background: "var(--app-input-bg)",
+  boxShadow: "var(--app-glow-soft)",
   flexShrink: 0,
 };
 
 const iconStyle: React.CSSProperties = {
-  width: 22,
-  height: 22,
+  width: 20,
+  height: 20,
   display: "block",
 };
 
