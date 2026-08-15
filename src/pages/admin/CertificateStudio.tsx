d763158e3a6064cc7d33e3055d5c632f32476ec4
panha <panhaeourn7@gmail.com>
2026-08-15 16:31:25 +0700

    Move certificate QR above frame

--- src/pages/admin/CertificateStudio.tsx
+++ src/pages/admin/CertificateStudio.tsx
@@ -45,7 +45,7 @@ const initialFieldSettings: FieldSettings = {
   issueDate: { font: "", size: "" },
 };
 
-const fixedQr: QrPlacement = { x: 7.1, y: 72.5, width: 12.2 };
+const fixedQr: QrPlacement = { x: 7.1, y: 71.2, width: 12.2 };
 const a4LandscapeWidthPx = (297 / 25.4) * 96;
 const publicSiteUrl = (import.meta.env.VITE_PUBLIC_SITE_URL || "https://cito.study").replace(/\/$/, "");
 
